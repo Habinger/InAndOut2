139e511b07747b1353f41fb692e2f269f0bd710d
Habinger <uchennachukwu16@gmail.com>
2022-02-21 16:17:40 +0100

    Add project files.

---
+++ InAndOut2/Controllers/AppointmetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InAndOut2.Controllers
+{
+    public class AppointmetController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+    }
+}
